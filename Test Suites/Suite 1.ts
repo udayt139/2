f6767b6aedfe8a06e2d22b229562c2dc1d02cfab
udayt139 <uday@totient.co.in>
2019-10-11 22:42:59 +0530

    1

---
+++ Test Suites/Suite 1.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4a9f89ad-58e0-4bdd-bbce-6cd40d849987</testSuiteGuid>
+   <testCaseLink>
+      <guid>59912a87-b69b-42d7-9ea6-f17ff34db0ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample/Create User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>55777806-9861-4e93-a1e5-7d7032feab10</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample/Delete User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3d925826-d966-4f6c-a11a-02d16974d88d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample/Get Single User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7b238039-8ee1-46a7-85ff-95c042e9c7ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample/Update User</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
